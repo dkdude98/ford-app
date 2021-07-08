b594bb2c07f278d910db6ed8c7b27b04821d5629
Darpun Kohli <dak190@pitt.edu>
2021-07-07 23:45:20 -0400

    Added templates folder and index.html

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return "Hello this is the new version!"
+	return render_template("index.html")
