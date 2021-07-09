e3d9c4608f5384770478df331afee46af6badc1c
Darpun Kohli <dak190@pitt.edu>
2021-07-09 00:28:05 -0400

    Heroku fixes

--- __init__.py
+++ __init__.py
@@ -3,3 +3,24 @@ from flask_sqlalchemy import SQLAlchemy
 
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = 'ford'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+    app.run(debug=True)
+
+    db.init_app(app)
+
+    # blueprint for auth routes in our app
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    # blueprint for non-auth parts of app
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
--- main.py
+++ main.py
@@ -4,31 +4,7 @@ import asyncio
 from aiohttp import ClientSession
 import myq
 from flask import Blueprint
-from flask_sqlalchemy import SQLAlchemy
-
-# init SQLAlchemy so we can use it later in our models
-db = SQLAlchemy()
-
-def create_app():
-    app = Flask(__name__)
-
-    app.config['SECRET_KEY'] = 'ford'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-    # app.config['TEMPLATES_AUTO_RELOAD'] = True
-    # app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-    # app.run(debug=True)
-
-    db.init_app(app)
-
-    # blueprint for auth routes in our app
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
-
-    # blueprint for non-auth parts of app
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
-
-    return app
+from . import db
 
 main = Blueprint('main', __name__)
 
