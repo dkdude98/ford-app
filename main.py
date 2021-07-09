2b85e39b09ac0ed4620f4251a14406f34f322b75
Darpun Kohli <dak190@pitt.edu>
2021-07-09 14:53:49 -0400

    Heroku

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 # main.py
-
 from flask import Blueprint, render_template
-from flask_login import login_required, current_user
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-main = Blueprint('main', __name__)
+# init SQLAlchemy so we can use it later in our models
+db = SQLAlchemy()
 
-@main.route('/')
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.run(debug=True)
+
+db.init_app(app)
+
+@app.route('/')
 def index():
     return render_template('sign-in.html')
 
-@main.route('/profile')
+@app.route('/profile')
 def profile():
     return 'Profile'
+
+@app.route('/login')
+def login():
+    return render_template("sign-in.html")
+
+@app.route('/signup')
+def signup():
+    return render_template('sign-up.html')
+
+@app.route('/logout')
+def logout():
+    return 'Logout'
