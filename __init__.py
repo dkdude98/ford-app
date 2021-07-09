7c3acb130c1729223ad02e69575fab68469b09b5
Darpun Kohli <dak190@pitt.edu>
2021-07-09 00:59:13 -0400

    Modified __init__.py

--- __init__.py
+++ __init__.py
@@ -13,6 +13,9 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 db.init_app(app)
 
+if __name__ == "main":
+    app.run(debug=True)
+
 # blueprint for auth routes in our app
 from .auth import auth as auth_blueprint
 app.register_blueprint(auth_blueprint)
@@ -20,6 +23,3 @@ app.register_blueprint(auth_blueprint)
 # blueprint for non-auth parts of app
 from .main import main as main_blueprint
 app.register_blueprint(main_blueprint)
-
-if __name__ == "main":
-    app.run(debug=True)
