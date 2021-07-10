ce8751759905008c179d63e1f9a30d21e8a5b397
Darpun Kohli <dak190@pitt.edu>
2021-07-10 19:59:10 -0400

    Working sign up, login, and profile pages

--- app.py
+++ app.py
@@ -1,28 +1,51 @@
 # main.py
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
-# init SQLAlchemy so we can use it later in our models
-db = SQLAlchemy()
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from flask_login import LoginManager
+from flask_login import login_user
+from flask_login import login_required, current_user, logout_user
 
 app = Flask(__name__)
-
-app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
+app.config['SECRET_KEY'] = 'secret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+# init SQLAlchemy so we can use it later in our models
+db = SQLAlchemy(app)
+db.create_all()
+
+db.metadata.clear()
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+
+db.metadata.clear()
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
 
 if __name__ == "main":
     app.run(debug=True)
 
 db.init_app(app)
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
 
 @app.route('/')
 def index():
     return render_template('sign-in.html')
 
 @app.route('/profile')
+@login_required
 def profile():
-    return 'Profile'
+    return render_template("blank.html",name=current_user.name)
 
 @app.route('/login')
 def login():
@@ -33,5 +56,53 @@ def signup():
     return render_template('sign-up.html')
 
 @app.route('/logout')
+@login_required
 def logout():
-    return 'Logout'
+    logout_user()
+    return redirect(url_for('index'))
+
+#####################################################################
+
+@app.route('/signup', methods=['POST'])
+def signup_post():
+    email = request.form.get('email')
+    name = request.form.get('name')
+    password = request.form.get('password')
+
+    user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
+
+    if user: # if a user is found, we want to redirect back to signup page so user can try again
+        flash('Email address already exists')
+        return redirect(url_for('signup'))
+
+    # create a new user with the form data. Hash the password so the plaintext version isn't saved.
+    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+
+    # add the new user to the database
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['POST'])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(email=email).first()
+
+    # check if the user actually exists
+    # take the user-supplied password, hash it, and compare it to the hashed password in the database
+    if not user or not check_password_hash(user.password, password):
+        flash('Please check your login details and try again.')
+        return redirect(url_for('login')) # if the user doesn't exist or password is wrong, reload the page
+    
+    login_user(user, remember=remember)
+    return redirect(url_for('profile'))
+
+######################################################################
+
+@login_manager.user_loader
+def load_user(user_id):
+    # since the user_id is just the primary key of our user table, use it in the query for the user
+    return User.query.get(int(user_id))
